350beea55abbb677ba891c7d76e470291af558c1
thaarani <sripavathaarani765@gmail.com>
2026-03-23 19:49:12 +0530

    Added app.js with code from terminal

---
+++ app.js
@@ -0,0 +1,14 @@
+// Simple JavaScript Program
+
+function greet(name) {
+    console.log("Hello " + name + "!");
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+greet("Pavathaarani");
+
+let result = add(10, 20);
+console.log("Sum is:", result);
